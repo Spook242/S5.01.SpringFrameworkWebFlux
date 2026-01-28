1340f786801788b9d8905be4a70c233e8b2c9dc8
Spook242 <spook242@gmail.com>
2026-01-28 15:31:41 +0100

    feat: clean code

--- src/main/java/cat/itacademy/blackjack/BlackjackApiApplication.java
+++ src/main/java/cat/itacademy/blackjack/BlackjackApiApplication.java
@@ -2,12 +2,8 @@ package cat.itacademy.blackjack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
-//@EnableR2dbcRepositories(basePackages = "cat.itacademy.blackjack.repository.mysql")
-//@EnableReactiveMongoRepositories(basePackages = "cat.itacademy.blackjack.repository.mongo")
 public class BlackjackApiApplication {
 
     public static void main(String[] args) {
